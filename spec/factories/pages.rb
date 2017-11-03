0545c6d42689a6fe43e50d692e346aa786f04ff1
praburangki <praburangki@gmail.com>
2017-11-03 09:12:14 +0800

    modify factories and add api spec

--- spec/factories/pages.rb
+++ spec/factories/pages.rb
@@ -1,5 +1,7 @@
 FactoryGirl.define do
   factory :page do
     page_url 'kumpul.co'
+
+    after(:create) { |page| create_list(:tag, 2, page: page) }
   end
 end
--- spec/factories/tags.rb
+++ spec/factories/tags.rb
@@ -1,7 +1,6 @@
 FactoryGirl.define do
   factory :tag do
-    content Faker::Lorem.sentence
-    content_type Tag.content_types.keys.sample.to_sym
-    page
+    content { Faker::Lorem.sentence }
+    content_type { Tag.content_types.keys.sample.to_sym }
   end
 end
